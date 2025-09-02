fb0047d6d65587d4cc4c01712172cb021b3034ad
gaeluwu29 <a24216872@alumnos.uady.mx>
2025-09-02 18:08:54 +0000

    dos objetos nuevos

--- src/org/ejercicio/banco/Main.java
+++ src/org/ejercicio/banco/Main.java
@@ -5,6 +5,6 @@ public class Main{
         CuentaBancaria cuenta1 = new CuentaBancaria(10000000, cliente1, 500000);
 
         Cliente cliente2 = new Cliente("Galelito1",100000);
-        CuentaBancaria cuenta2 = new CuentaBancaria(123456, cliente2, 23446);
+        CuentaBancaria cuenta2 = new CuentaBancaria(123456, cliente2,1200);
     }
 }
